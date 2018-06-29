d1146dfff28d23585bae7bff6b38f526f8fe4699
Jimmy McNamara <jmcnamara44@gmail.com>
2018-06-29 13:54:51 -0700

    add a custom error message if an invalid url path is typed in. it will also add an image to the header page.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import Header from './Header';
 import BeerList from './BeerList';
 import {Switch, Route } from 'react-router-dom';
 import NewBeerForm from './NewBeerForm';
+import Error404 from './Error404';
 
 function App(){
   return (
@@ -11,6 +12,7 @@ function App(){
       <Switch>
         <Route exact path='/' component={BeerList} />
         <Route path='/newbeer' component={NewBeerForm} />
+        <Route component={Error404} />
       </Switch>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import taproom from '../assets/images/taproom.jpg';
 
 function Header(){
   return (
     <div>
+      <img src={taproom}/>
       <h1>Taproom</h1>
       <Link to="/">Home</Link> | <Link to="/newbeer">Add a New Beer</Link>
     </div>
