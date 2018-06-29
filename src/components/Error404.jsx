87e5ad413c748669d9d739ad1055d92e5647b502
Jimmy McNamara <jmcnamara44@gmail.com>
2018-06-29 14:05:40 -0700

    add a proptype for location in error404.

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 function Error404(props){
   return (
@@ -7,7 +8,10 @@ function Error404(props){
       <h2>The page {props.location.pathname} does not exist.</h2>
       <h3>Would you like to return <Link to="/">home</Link>?</h3>
     </div>
-
   );
 }
- export default Error404;
+
+Error404.propTypes = {
+  location: PropTypes.object
+};
+export default Error404;
